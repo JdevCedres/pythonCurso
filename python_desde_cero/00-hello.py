5e555133bc8d8a35c702e3dd9c065be5de5f52b6
JdevCedres <jdgc.damaso@icloud.com>
2023-10-05 21:07:27 +0100

    variables

---
+++ python_desde_cero/00-hello.py
@@ -0,0 +1,17 @@
+# Hola mundo esto es un comentario
+
+""" 
+Esto es un comentarion 
+y esto es multilinea 
+con lo que quieras 
+"""
+
+print("Hola python")
+print('Hola python con comillas simple')
+
+# Consultar tipos de datos 
+
+print(type("Soy un dato str"))
+print(type(5)) # Es un dato int
+print(type(1.2)) # Es un dato float
+print(type(True)) # Es un dato bool. 
---
+++ python_desde_cero/01-variables.py
@@ -0,0 +1,44 @@
+# Variable
+
+my_variable = "My string variable"   # Para definir una variable snake_case, no tiene palabra reservada.
+print(my_variable)
+my_int_variable = 5
+print(my_int_variable)
+my_bool_variable = True   # en los booleanos siempre en mayusculas True False
+print(my_bool_variable)
+
+# Concatenación de variables en un print
+print(my_variable, my_int_variable, my_bool_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+
+print("Este es el valor de:", my_bool_variable)
+
+# función de sistemas
+#  'len()' cuenta los caracteres incluido los espacios
+
+print(len(my_variable))
+
+
+# Variables en  una sola linea. ¡No se suele usar!
+name, surname, alias, age = "Jose", "Gonzalez", "Jdev", 46
+print("Me llamo:",name, surname, ". Mi edad es:", age, ". y mi alias es:", alias)
+
+# Inputs 
+'''
+first_name = input('What is your name: ')
+age_input = input('How old are you? ')
+print(first_name)
+print(age_input)
+'''
+
+# Cambiamos el tipo Python es de tipado debil, por lo que puedes cambiar el tipo a lo que quieras. CUIDADO
+name = 45
+age = "Jose"
+
+print(name)
+print(age)
+
+
+
