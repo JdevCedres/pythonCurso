3cfba67b2e045f66dca1d4a96600030a63d79d15
JoseDev <jdevmov@gmail.com>
2023-10-10 17:49:14 +0100

    funtions

--- python_desde_cero/10-functions.py
+++ python_desde_cero/10-functions.py
@@ -1,9 +1,46 @@
 # Funciones
 
-def my_funtion ():
+def my_funtion():
     print("Esto es una función")
 
+my_funtion()
+
+# Función con parametros
+
+def sum_values (first_number, second_number):
+    print(first_number + second_number)
+
+sum_values(5, 7)
+
+#FUnción con parametros de salida
+
+def sum_values_return (first_value, second_value):
+    return first_value + second_value
+
+result = sum_values_return(10, 5)
+print(result)
+
+def print_name(name, surname):
+    print(f"{name} {surname}")
 
 
+print_name(surname="González", name= "José")
 
-my_funtion ()
+# función con parametros por defectos.
+
+def print_name_default(name, surname, alias = "Sin alias"):
+    print(f"{name} {surname} {alias}")
+
+
+print_name_default("Jose","Gonzalez", "Jdev")
+print_name_default("Jose","Gonzalez")
+
+# se imprime varios textos varios textos 
+
+def print_texts(*texts):
+    for text in texts:
+        print(text.upper())
+
+
+print_texts("Hola", "Jose es el mejor", "Se acabo")
+
