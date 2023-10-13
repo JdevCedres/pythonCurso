7e49daf378686a6f21a2dfc1ae67f18f1421ebfd
JoseDev <jdevmov@gmail.com>
2023-10-13 16:42:01 +0100

    Change to basic

--- python_desde_cero/13-modules.py
+++
@@ -1,5 +0,0 @@
-# Modules
-import module # importa todo el modulo todo lo que hay en el archivo.
-# from module import sum ... de esta manera lo haces por función en este caso. 
-
-module.sum(10, 12, 5)
---
+++ python_desde_cero/Basic/13-modules.py
@@ -0,0 +1,3 @@
+# Modules
+import python_desde_cero.Basic.module as module # importa todo el modulo todo lo que hay en el archivo.
+# from module import sum ... de esta manera lo haces por función en este caso. 
